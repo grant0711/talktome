11e562e17636efe004a91f93742c82bd9c1cd17a
grant0711 <adamsgrant87@gmail.com>
2022-06-01 11:58:15 +0000

    Remove decode from hmac.digest

--- api/auth.py
+++ api/auth.py
@@ -10,7 +10,7 @@ def authenticate_webhook_request(logger, x_hub_signature, payload):
     
     """
 
-    test_signature = hmac.digest(os.environ['WEBHOOK_SECRET'].encode(), payload, 'sha1').decode('utf-8')
+    test_signature = hmac.digest(os.environ['WEBHOOK_SECRET'].encode(), payload, 'sha1')
     print(test_signature)
     print(x_hub_signature)
 
