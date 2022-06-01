133f29ee08fcd3e63f3f6359a4526b74af474495
grant0711 <adamsgrant87@gmail.com>
2022-06-01 15:53:47 +0000

    Set missing fields to None on event insert

--- api/event_handler.py
+++ api/event_handler.py
@@ -9,44 +9,17 @@ def insert_incoming_event(logger, message_info, contact_info):
     logger.debug(f"MESSAGE INFO : {message_info}")
     logger.debug(f"CONTACT INFO : {contact_info}")
 
+    # Transform our dicts into strings for JSONB insert or set as None
+    text = "'"+json.dumps(message_info.get("text"))+"'" if message_info.get("text") else None
+    image = "'"+json.dumps(message_info.get("image"))+"'" if message_info.get("image") else None
+    audio = "'"+json.dumps(message_info.get("audio"))+"'" if message_info.get("audio") else None
+    document = "'"+json.dumps(message_info.get("document"))+"'" if message_info.get("document") else None
+
     sql = f"""
-        INSERT INTO whatsapp.events (
-            message_id,
-            timestamp,
-            type,
-            text,
-            image,
-            audio,
-            document,
-            direction,
-            automated,
-            contact_id) VALUES (
-                '{message_info["id"]}',
-                '{message_info["timestamp"]}',
-                '{message_info["type"]}',
-                '{json.dumps(message_info.get("text", {}))}',
-                '{json.dumps(message_info.get("image", {}))}',
-                '{json.dumps(message_info.get("audio", {}))}',
-                '{json.dumps(message_info.get("document", {}))}',
-                'inbound',
-                FALSE,
-                '{contact_info["wa_id"]}'
-            ) RETURNING message_id;
+        INSERT INTO whatsapp.events (message_id, timestamp, type, text, image, audio, document, direction, automated, contact_id)
+        VALUES ('{message_info["id"]}', '{message_info["timestamp"]}', '{message_info["type"]}', {text}, {image}, {audio}, {document}', 'inbound', FALSE, '{contact_info["wa_id"]}')
+        RETURNING message_id;
     """
     message_id = async_postgres.execute(logger, 'heroku', sql, commit=True)
-
-    #sample_contact_info = {
-    #    'wa_id': '221784269198',
-    #    'time_created': datetime.datetime(2022, 6, 1, 13, 45, 37, 248582),
-    #    'vars': None
-    #}
-    
-    #sample_message_info = {
-    #    'from': '221784269198',
-    #    'id': 'wamid.HBgMMjIxNzg0MjY5MTk4FQIAEhggNkY3Q0MwREU2REQ2RkM5NzlCMzU2QjcyQjYxODgyMkEA',
-    #    'timestamp': '1654091136',
-    #    'text': {
-    #        'body': 'Test21'
-    #    },
-    #    'type': 'text'
-    #}
+    logger.info(message_id)
+    return message_id
