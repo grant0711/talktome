38a16ecc5b7f3bab8bb278a5e3fbacc6669f9e15
grant0711 <adamsgrant87@gmail.com>
2022-06-01 12:11:42 +0000

    Added unicode escape encode to payload

--- api/auth.py
+++ api/auth.py
@@ -10,8 +10,8 @@ def authenticate_webhook_request(logger, x_hub_signature, payload):
     
     """
 
-    test_signature = hmac.digest(os.environ['WEBHOOK_SECRET'].encode(), payload, 'sha1')
-    test_signature = base64.b64encode(test_signature).decode('utf-8')
+    test_signature = hmac.new(os.environ['WEBHOOK_SECRET'].encode(), payload.encode('unicode-escape'), 'sha1')
+    test_signature = base64.b64encode(test_signature.digest()).decode('utf-8')
 
     print(test_signature)
     print(x_hub_signature)
